7248df5f5a7286ba14356dd6f2f86bcfddee8cd5
Majon1 <mathilda.nynas@gmail.com>
2020-04-13 13:55:12 +0300

    example added

---
+++ test/ExampleTests/ExampleTests.cs
@@ -0,0 +1,31 @@
+using System;
+using NUnit.Framework;
+using Animals;
+
+public class PetTests
+{
+  [Test]
+  public void DogTalkToOwnerReturnsWoof()
+  {
+    string expected = "Woof!";
+    string actual = new Dog().TalkToOwner();
+
+    Assert.AreEqual(expected, actual, "Dog should say Woof!");
+  }
+  [Test]
+  public void CatTalkToOwnerReturnsWoof()
+  {
+    string expected = "Mew!";
+    string actual = new Cat().TalkToOwner();
+
+    Assert.AreEqual(expected, actual, "Cat should say Mew!");
+  }
+[Test]
+  public void BirdTalkToOwnerReturnsSqueek()
+  {
+    string expected = "Chirp!";
+    string actual = new Bird().TalkToOwner();
+
+    Assert.AreEqual(expected, actual, "Bird should say Chirp!");
+  }
+}
