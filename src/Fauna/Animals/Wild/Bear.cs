0be698c3817b08f4f2e7f2468703fef19992adcb
Daniel Tunér <tuner.daniel@hotmail.com>
2020-05-13 19:27:11 +0300

    first try to inplement hunt

--- src/Fauna/Animals/Wild/Bear.cs
+++ src/Fauna/Animals/Wild/Bear.cs
@@ -4,13 +4,35 @@ namespace Animals
 {
   public class Bear : Animal, IWild
   {
+    public int weight { get; set; }
+    private Random random;
+
+    public Bear()
+    {
+      this.random = new Random();
+      this.weight = 300;     
+    }
     public override string MakeSound()
     {
       return "Growl!";
     }
     public string Hunt()
     {
-        return "Bear hunts!";
-    } 
+      double probability = this.random.NextDouble();
+
+      if (probability <= 0.6)
+      {
+        Eat();
+        return "Bear kills and eats and now weighs " + this.weight + " kg.";
+      }
+      else
+      {
+        return "Found no game";
+      }
+    }
+    public override void Eat()
+    {
+      this.weight += 1;
+    }    
   }
 }
